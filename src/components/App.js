20308e3c7085940dead74f7fa28a30f7c1f22174
rayGreycloud <rayGreycloud@gmail.com>
2018-12-29 12:39:06 -0800

    Add UserList component using hook

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 import ResourceList from './ResourceList';
+import UserList from './UserList';
 
 const App = () => {
   const [resource, setResource] = useState('posts');
@@ -10,8 +11,10 @@ const App = () => {
       <div>
         <button onClick={() => setResource('posts')}>Posts</button>
         <button onClick={() => setResource('todos')}>Todos</button>
+        <button onClick={() => setResource('users')}>Users</button>
       </div>
-      <ResourceList resource={resource} />
+      {resource !== 'users' && <ResourceList resource={resource} />}
+      {resource === 'users' && <UserList />}
     </div>
   );
 };
